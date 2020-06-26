eeb83a9b36051ad69c4df9d69b9e968e60c6687c
Azrood <53349565+Azrood@users.noreply.github.com>
2020-06-26 20:25:51 +0000

    Update solve.py

--- nahamconCTF/homecooked/solve.py
+++ nahamconCTF/homecooked/solve.py
@@ -1,20 +1,6 @@
 import base64
 num = 0
 cipher_b64 = b"MTAwLDExMSwxMDAsOTYsMTEyLDIxLDIwOSwxNjYsMjE2LDE0MCwzMzAsMzE4LDMyMSw3MDIyMSw3MDQxNCw3MDU0NCw3MTQxNCw3MTgxMCw3MjIxMSw3MjgyNyw3MzAwMCw3MzMxOSw3MzcyMiw3NDA4OCw3NDY0Myw3NTU0MiwxMDAyOTAzLDEwMDgwOTQsMTAyMjA4OSwxMDI4MTA0LDEwMzUzMzcsMTA0MzQ0OCwxMDU1NTg3LDEwNjI1NDEsMTA2NTcxNSwxMDc0NzQ5LDEwODI4NDQsMTA4NTY5NiwxMDkyOTY2LDEwOTQwMDA="
-
-def a(num):
-    if num == 2: return True
-    if (num > 1) and num % 2 != 0:
-        for i in range(3,num,2):
-            if (num % i) == 0:
-                return False
-        return True
-    else:
-        return False
-       
-def b(num):
-    return str(num) == str(num)[::-1]
-
 # get palindromic primes from sequence
 cipher = base64.b64decode(cipher_b64).decode().split(",")
 primes = [2,3,5,7,11,101,131,151,181,191,313,353,373,70207,
